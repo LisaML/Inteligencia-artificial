5c1a507034bfe99dacf2959b31ebfb26aa7aa15d
LisaML <lisa_janeth@hotmail.com>
2018-05-02 15:53:28 -0600

    tokens con tres oraciones

--- tokens.py
+++ tokens.py
@@ -5,60 +5,62 @@ from sklearn.naive_bayes import MultinomialNB
 stopwords = ['la','de','el','podrias','por','favor']
 
 if __name__ == '__main__':
-	sentence= 'Podiras reproducirla cancion de el noa noa , por favor !'
+	sentence=['Podiras reproducirla cancion de el noa noa por favor !','busca la pelicula de dracula', 'Por favor reproduce el audiolibro']
+	for x in sentence:
+		sentence=x.split(',')
+		for se in sentence:
+			#tokenizar
+			tokens = se.split(' ')
 
-	#tokenizar
-	tokens = sentence.split(' ')
+			#sentence cleaning
 
-	#sentence cleaning
+			clean_token=[]
 
-	clean_token=[]
+			for token in tokens:
 
-	for token in tokens:
+				#remove punctuation
+				if all(char in set(string.punctuation) for char in token):
+					continue
 
-		#remove punctuation
-		if all(char in set(string.punctuation) for char in token):
-			continue
+				#remove numbers
+				if token.isdigit():
+					continue
 
-		#remove numbers
-		if token.isdigit():
-			continue
+				#transform the token to lowcase and remove sentences
+				token= token.lower()
+				token = token.strip()
 
-		#transform the token to lowcase and remove sentences
-		token= token.lower()
-		token = token.strip()
+				#remove stopworld
+				if token in stopwords:
+					continue
 
-		#remove stopworld
-		if token in stopwords:
-			continue
+				
 
-		
+				clean_token.append(token)
 
-		clean_token.append(token)
+			#print(tokens)
+			#print(clean_token)
 
-	#print(tokens)
-	#print(clean_token)
-
-	#solo en la procatica
-	temp=[]
-	temp.append(' '.join(clean_token))
-
-
-	#bag of words transformation
-	conunt_vect = CountVectorizer()
-	bag_of_words_array = conunt_vect.fit_transform(temp)
-
-	print(bag_of_words_array)
-	print(conunt_vect.vocabulary_)
-
-	#model training
-	naive_bayes_classifier = MultinomialNB()
-	naive_bayes_classifier.fit(bag_of_words_array,['1'])
-
-	print(naive_bayes_classifier.predict(bag_of_words_array))
+			#solo en la procatica
+			temp=[]
+			temp.append(' '.join(clean_token))
 
 
-####SUBIR EL ARCHIVO A GIT########
+			#bag of words transformation
+			conunt_vect = CountVectorizer()
+			bag_of_words_array = conunt_vect.fit_transform(temp)
+
+			print(bag_of_words_array)
+			print(conunt_vect.vocabulary_)
+
+			#model training
+			naive_bayes_classifier = MultinomialNB()
+			naive_bayes_classifier.fit(bag_of_words_array,['1'])
+
+			print(naive_bayes_classifier.predict(bag_of_words_array))
+
+
+
 #modificar para que aprenda 3 oraciones
 #creando un arreglo de oraciones
 #para el predict poner un string
